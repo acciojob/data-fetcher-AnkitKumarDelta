bc45f88f03def16f64fc683c363c44ef379c86bb
AnkitKumarDelta <ankitna355@gmail.com>
2025-12-14 11:18:35 +0530

    commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,27 @@
 
-import React from "react";
+import React, { useEffect, useState } from "react";
 import './../styles/App.css';
 
 const App = () => {
+  const[data, setData] = useState(null);
+
+  useEffect(()=>{
+    fetch('https://dummyjson.com/products')
+    .then(res=>res.json())
+    .then(res=>setData(res))
+    .catch(err=>console.log(err));
+  })
+
   return (
     <div>
-        {/* Do not remove the main div */}
+        {data!=null
+         ?<div>
+          <h1>Data fetched from API</h1>
+         <pre>
+        {JSON.stringify(data)}
+        </pre>
+         </div>
+         : (<p>Loading...</p>)}
     </div>
   )
 }
