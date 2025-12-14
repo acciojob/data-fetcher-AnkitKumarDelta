d35f7cf89c6c3952accd6366e2a5a98e267c0a4e
AnkitKumarDelta <ankitna355@gmail.com>
2025-12-14 11:23:53 +0530

    save changes

--- src/components/App.js
+++ src/components/App.js
@@ -1,29 +1,43 @@
-
 import React, { useEffect, useState } from "react";
 import './../styles/App.css';
 
 const App = () => {
-  const[data, setData] = useState(null);
+  const [data, setData] = useState(null);
+  const [error, setError] = useState(null);
 
-  useEffect(()=>{
-    fetch('https://dummyjson.com/products')
-    .then(res=>res.json())
-    .then(res=>setData(res))
-    .catch(err=>console.log(err));
-  })
+  useEffect(() => {
+    fetch("https://dummyjson.com/products")
+      .then((res) => res.json())
+      .then((res) => {
+        if (!res || res.length === 0) {
+          setData([]); 
+        } else {
+          setData(res);
+        }
+      })
+      .catch((err) => {
+        setError("An error occurred: " + err.message);
+      });
+  }, []); 
+
+  if (error) {
+    return <p>{error}</p>;
+  }
+
+  if (data === null) {
+    return <p>Loading...</p>;
+  }
+
+  if (Array.isArray(data) && data.length === 0) {
+    return <p>No data found</p>; 
+  }
 
   return (
     <div>
-        {data!=null
-         ?<div>
-          <h1>Data fetched from API</h1>
-         <pre>
-        {JSON.stringify(data)}
-        </pre>
-         </div>
-         : (<p>Loading...</p>)}
+      <h1>Data Fetched from API</h1>
+      <pre>{JSON.stringify(data, null, 2)}</pre>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
